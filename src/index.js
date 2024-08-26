e90a1921fed0aa72b877f39de60ea923642baae1
Djm <dajaramillo@unicauca.edu.co>
2024-08-26 12:52:48 -0500

    Se crearon rutas

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ const express = require('express')
 
 //configuraciones
 const app = express()
-const port = 3000
+app.set('port', process.env.PORT || 3000);
+
 
 //rutas
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(require('./routes/index'));
 
 //Servidor
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening on port`, app.get('port'))
+});
